81a6d47c0d4ed7bd6a664fbae2b6d2c9cc80be4d
Jokerbm <37797569+Jokerbm@users.noreply.github.com>
2022-02-27 23:47:01 +0700

    g

--- test.cc
+++ test.cc
@@ -1,11 +1,11 @@
 // หน้าการ = 95 % ลดอัตรการแพร่เชื้อ 90 %
-// วัคซีน =
-// 1. shinovac 2 เข็ม กัน 51% 
-// ประชาชน 7คน #
-// 2. astra 2 เข็มกัน 79% 
-// ประชาชน 2คน #
+// วัคซีน
+// 1. shinovac 2 เข็ม กัน 51%
+// ประชาชน 7คน
+// 2. astra 2 เข็มกัน 79%
+// ประชาชน 2คน
 // ทหาร 10คน #
-// 3. pfizer 2 เข็มกัน 95%  
+// 3. pfizer 2 เข็มกัน 95%
 // ทหาร 10คน #
 // 4. moderna 2  94%
 // ประชาชน 1คน #
@@ -16,7 +16,7 @@
 // 7. คนติดโควิต
 // ประชาชน 1 คน #
 // 8. unvaccineted
-// ประชาชน 26คน # 
+// ประชาชน 26คน #
 
 #include <ostream>
 #include "ns3/applications-module.h"
@@ -26,15 +26,17 @@
 #include "ns3/mobility-module.h"
 #include "ns3/netanim-module.h"
 
-
-#define DURATION 10.0 // เวลาจำลอง 10 วินาที
-#define N 100 // จำนวนคนทั้งหมด
-#define M 80 // จำนวนลูกค้า
-#define X_BOX 100 // กว้างแนวนอน
-#define Y_BOX 50 // กว้างแนวตั้ง
-#define INFECTRAD 2 // ระยะห่างที่ปลอดภัย (จำลอง 2 เมตร)
+// ตัวแปรประแภท วัคซีนและประชากร
+#define TOTAL_MAN 100
+#define PEOPLE_SHINOVAC 7
+#define PEOPLE_ASTRA 2
+#define SOLIDER_ASTRA 10
+#define DURATION 10.0    // เวลาจำลอง 10 วินาที
+#define X_BOX 100        // กว้างแนวนอน
+#define Y_BOX 50         // กว้างแนวตั้ง
+#define INFECTRAD 2      // ระยะห่างที่ปลอดภัย (จำลอง 2 เมตร)
 #define INFECTCHANCE 1.5 // โอกาสติด 1.5%
-#define NodeSide 1.0 // ขนาดของจุดใน netanim
+#define NodeSide 1.0     // ขนาดของจุดใน netanim
 
 // list คนที่อยากให้ติดเชื้อ เลือกค่าใน array ได้ 0 <= infected_list[i] < N
 // โดยท่ีค่า 0 <= infected_list[i] < M จะเป็นลูกค้า
@@ -46,26 +48,28 @@ int infected_list[] = {0, 1, 2, 3, 4};
 using namespace ns3;
 using namespace std;
 
-struct rgb {
-  uint8_t r; 
-  uint8_t g; 
-  uint8_t b; 
+struct rgb
+{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
 };
 
-struct rgb colors [] = {
-  { 252, 44, 44 }, // ติดเชื้อ ประชาชน 1คน
-  { 127, 255, 46 }, // sol astra 10 คน
-  {255, 0, 0} // sinovac 2 ประชาชน 7คน 
-  {255, 255, 0} // astra 2 ประชาชน 2คน 
-  {255, 126, 172} // phizer 2 ทหาร 10 คน
-  {135, 190, 219} // moderna 2 ประชาชน 1 คน
-  {216, 133, 219} // si+as ประชาชน 37 คน
-  {162, 218, 143} // as+phi ประชาชน 6 คน
-  {77, 254, 51} // unvacc ประชาชน 26คน 
+struct rgb colors[] = {
+    {255, 0, 0},   // sinovac 2 ประชาชน 7คน
+    {255, 255, 0},   // astra 2 ประชาชน 2คน
+    {127, 255, 46}, // sol astra 10 คน
+    {252, 44, 44},  // ติดเชื้อ ประชาชน 1คน
+    {255, 126, 172}, // phizer 2 ทหาร 10 คน
+    {135, 190, 219}, // moderna 2 ประชาชน 1 คน
+    {216, 133, 219}, // si+as ประชาชน 37 คน
+    {162, 218, 143}, // as+phi ประชาชน 6 คน
+    {77, 254, 51}   // unvacc ประชาชน 26คน
 };
 
-class People {
-  public:
+class People
+{
+public:
     int people_count, customer_count;
     NodeContainer node;
     NetDeviceContainer device;
@@ -84,66 +88,71 @@ class People {
     void setUDPClient(int people_id, Time startTime);
     void setMobility();
     bool receiveCOVID(
-      Ptr<NetDevice> dst_device, 
-      Ptr<const Packet> packet, 
-      uint16_t protocol,
-      const Address &src, 
-      const Address &dst, 
-      BridgeNetDevice::PacketType packetType);
+        Ptr<NetDevice> dst_device,
+        Ptr<const Packet> packet,
+        uint16_t protocol,
+        const Address &src,
+        const Address &dst,
+        BridgeNetDevice::PacketType packetType);
     int getNodeIdFromAddress(const Address &src);
 };
 
-// สร้างคน 100 คน 
-People people(N, M);
-AnimationInterface * pAnim = 0;
-bool is_infected[N] = {false};
+// สร้างคน 100 คน
+People people(PEOPLE_SHINOVAC, TOTAL_MAN );
+AnimationInterface *pAnim = 0;
+bool is_infected[PEOPLE_SHINOVAC] = {false};
 
-People::People(int people, int customer) {
-  people_count = people;
-  customer_count = customer;
+People::People(int people, int customer)
+{
+    people_count = people;
+    customer_count = customer;
 
-  // สร้าง node จำนวน people โหนด
-  node.Create (people_count);
+    // สร้าง node จำนวน people โหนด
+    node.Create(20);
 
-  // ติดตั้ง internet stack
-  stack.Install (node);
+    // ติดตั้ง internet stack
+    stack.Install(node);
 }
 
-void People::setCSMA(int dataRate, int delay, int mtu) {
-  csma.SetChannelAttribute ("DataRate", DataRateValue (DataRate (dataRate)));
-  csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (delay)));
-  csma.SetDeviceAttribute ("Mtu", UintegerValue (mtu));
-  device = csma.Install (node);
+void People::setCSMA(int dataRate, int delay, int mtu)
+{
+    csma.SetChannelAttribute("DataRate", DataRateValue(DataRate(dataRate)));
+    csma.SetChannelAttribute("Delay", TimeValue(MilliSeconds(delay)));
+    csma.SetDeviceAttribute("Mtu", UintegerValue(mtu));
+    device = csma.Install(node);
 
-  // ตั้งค่า handler กรณีมี packet เข้ามา
-  // NetDeviceContainer::Iterator i;
-  // for (i = device.Begin (); i != device.End (); ++i) {
-  //   (*i)->SetPromiscReceiveCallback (MakeCallback (&People::receiveCOVID, this));  // some NetDevice method
-  // }
+    // ตั้งค่า handler กรณีมี packet เข้ามา
+    // NetDeviceContainer::Iterator i;
+    // for (i = device.Begin (); i != device.End (); ++i) {
+    //   (*i)->SetPromiscReceiveCallback (MakeCallback (&People::receiveCOVID, this));  // some NetDevice method
+    // }
 }
 
-void People::setIPV4(string address, string netmask) {
-  ipv4.SetBase (Ipv4Address(address.c_str()), Ipv4Mask(netmask.c_str()));
-  interface = ipv4.Assign (device);
-  serverAddress = Address(interface.GetAddress (1));
+void People::setIPV4(string address, string netmask)
+{
+    ipv4.SetBase(Ipv4Address(address.c_str()), Ipv4Mask(netmask.c_str()));
+    interface = ipv4.Assign(device);
+    serverAddress = Address(interface.GetAddress(1));
 }
 
-void People::setUDPClient(int people_id, Time startTime) { // เสมือนติดตั้งตัวแพร่เชื้อ (ให้เฉพาะคนติดโควิด)
-  uint32_t packetSize = 1024;
-  uint32_t maxPacketCount = 1000;
-  Time interPacketInterval = MilliSeconds (50);
-  UdpEchoClientHelper client (serverAddress, port);
-  client.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
-  client.SetAttribute ("Interval", TimeValue (interPacketInterval));
-  client.SetAttribute ("PacketSize", UintegerValue (packetSize));
-  
-  // ถ้าติดก็เปลี่ยนสีพร้อมลงตัวแพร่เชื้อ
-  if (is_infected[people_id] == false) {
-    is_infected[people_id] = true;
-    apps = client.Install (node.Get (people_id));
-    apps.Start (startTime);
-    apps.Stop (Seconds (DURATION));
-  }
+void People::setUDPClient(int people_id, Time startTime)
+{ // เสมือนติดตั้งตัวแพร่เชื้อ (ให้เฉพาะคนติดโควิด)
+    uint32_t packetSize = 1024;
+    uint32_t maxPacketCount = 1000;
+    Time interPacketInterval = MilliSeconds(50);
+    UdpEchoClientHelper client(serverAddress, port);
+    client.SetAttribute("MaxPackets", UintegerValue(maxPacketCount));
+    client.SetAttribute("Interval", TimeValue(interPacketInterval));
+    client.SetAttribute("PacketSize", UintegerValue(packetSize));
+
+    // ถ้าติดก็เปลี่ยนสีพร้อมลงตัวแพร่เชื้อ
+    // if (is_infected[people_id] == false)
+    // {
+    //     is_infected[people_id] = true;
+    //     apps = client.Install(node.Get(people_id));
+    //     apps.Start(startTime);
+    //     apps.Stop(Seconds(DURATION));
+    // }
 }
 
 // void People::setMobility() {
@@ -170,7 +179,7 @@ void People::setUDPClient(int people_id, Time startTime) { // เสมือน
 //     "Z", StringValue ("ns3::UniformRandomVariable[Min=0|Max=10]"));
 
 //   for (int i = 0; i < customer_count; i++) {
-//     mobility_move.Install (node.Get (i)); 
+//     mobility_move.Install (node.Get (i));
 //   } for (int i = customer_count; i < people_count; i++) {
 //     mobility_nomove.Install (node.Get (i));
 //   }
@@ -178,11 +187,11 @@ void People::setUDPClient(int people_id, Time startTime) { // เสมือน
 
 // ถ้าได้รับ packet จะมาทำ function นี้ ใช้คำนวณโอกาสติดได้
 // bool People::receiveCOVID (
-//     Ptr<NetDevice> dst_device, 
-//     Ptr<const Packet> packet, 
+//     Ptr<NetDevice> dst_device,
+//     Ptr<const Packet> packet,
 //     uint16_t protocol,
-//     const Address &src, 
-//     const Address &dst, 
+//     const Address &src,
+//     const Address &dst,
 //     BridgeNetDevice::PacketType packetType) {
 
 //   // เช็คดูว่าเป็น UDP ไหม (ดูขนาด packet) เพื่อกรอง arp ออก
@@ -217,7 +226,7 @@ void People::setUDPClient(int people_id, Time startTime) { // เสมือน
 
 //     }
 //   }
-  
+
 //   return true;
 // }
 
@@ -233,52 +242,62 @@ void People::setUDPClient(int people_id, Time startTime) { // เสมือน
 //   return found;
 // }
 
-int main (int argc, char *argv[]) {
-  CommandLine cmd (__FILE__);
-  cmd.Parse (argc, argv);
+int main(int argc, char *argv[])
+{
+    CommandLine cmd(__FILE__);
+    cmd.Parse(argc, argv);
 
-  // setCSMA -> DataRate, Delay, Mtu
-  people.setCSMA(5000000, 0, 1400);
+    // setCSMA -> DataRate, Delay, Mtu
+    people.setCSMA(5000000, 0, 1400);
 
-  // setIPV4 -> ip, netmask
-  people.setIPV4("10.10.100.0", "255.255.255.0");
+    // setIPV4 -> ip, netmask
+    people.setIPV4("10.10.100.0", "255.255.255.0");
 
-  // แยกพ่อค้ากับลูกค้า (เคลื่อนไหวได้กับไม่ได้)
-  // people.setMobility();
+    // แยกพ่อค้ากับลูกค้า (เคลื่อนไหวได้กับไม่ได้)
+    // people.setMobility();
 
-  // ตั้ง udp client ตัวแพร่เชื้อ -> คนที่ติดเชื้อ 
-  // int arr_size = sizeof(infected_list)/sizeof(infected_list[0]);
-  // for (int i = 0; i < arr_size; i++) {
-  //   people.setUDPClient(infected_list[i], Seconds(0.0));
-  // }
+    // ตั้ง udp client ตัวแพร่เชื้อ -> คนที่ติดเชื้อ
+    // int arr_size = sizeof(infected_list)/sizeof(infected_list[0]);
+    // for (int i = 0; i < arr_size; i++) {
+    //   people.setUDPClient(infected_list[i], Seconds(0.0));
+    // }
 
-  Simulator::Schedule (Seconds (0.00),
-    [] ()
-    {
-      for (int i = 0; i < M; i++) {
-        pAnim->UpdateNodeSize (i, NodeSide, NodeSide);
-        pAnim->UpdateNodeColor (people.node.Get(i), colors[1].r, colors[1].g, colors[1].b);
-      } for (int i = M; i < N; i++) {
-        pAnim->UpdateNodeSize (i, NodeSide, NodeSide);
-        pAnim->UpdateNodeColor (people.node.Get(i), colors[2].r, colors[2].g, colors[2].b);
-      }
+    Simulator::Schedule(Seconds(0.00),
+                        []()
+                        {
+                            for (int z = 1; z < PEOPLE_SHINOVAC + 1; z++)
+                            {
+                                pAnim->UpdateNodeSize(z, NodeSide, NodeSide);
+                                pAnim->UpdateNodeColor(people.node.Get(z), colors[0].r, colors[0].g, colors[0].b);
+                            }
+                            for (int x = z; x < z +PEOPLE_ASTRA + 1; x++)
+                            {
+                                pAnim->UpdateNodeSize(x, NodeSide, NodeSide);
+                                pAnim->UpdateNodeColor(people.node.Get(x), colors[1].r, colors[1].g, colors[1].b);
+                            }
+                            for (int c = x c < x + SOLIDER_ASTRA +1; c++)
+                            {
+                                pAnim->UpdateNodeSize(c, NodeSide, NodeSide);
+                                pAnim->UpdateNodeColor(people.node.Get(c), colors[1].r, colors[1].g, colors[1].b);
+                            }
 
-      int arr_size = sizeof(infected_list)/sizeof(infected_list[0]);
-      for (int i = 0; i < arr_size; i++) {
-        pAnim->UpdateNodeColor (people.node.Get(infected_list[i]), colors[0].r, colors[0].g, colors[0].b);
-      }
-    });
-  
-  // จบ Simulation
-  Simulator::Stop (Seconds (DURATION));
+                            // int arr_size = sizeof(infected_list) / sizeof(infected_list[0]);
+                            // for (int i = 0; i < arr_size; i++)
+                            // {
+                            //     pAnim->UpdateNodeColor(people.node.Get(infected_list[i]), colors[0].r, colors[0].g, colors[0].b);
+                            // }
+                        });
 
-  // ไฟล์ NetAnimation
-  pAnim = new AnimationInterface ("covid-model.xml");
-  // pAnim->EnablePacketMetadata (true); // แสดงประเภท packet บนลูกศร
-  pAnim->SetMaxPktsPerTraceFile (1000000);
+    // จบ Simulation
+    Simulator::Stop(Seconds(DURATION));
 
-  Simulator::Run ();
-  Simulator::Destroy ();
+    // ไฟล์ NetAnimation
+    pAnim = new AnimationInterface("covid-model.xml");
+    // pAnim->EnablePacketMetadata (true); // แสดงประเภท packet บนลูกศร
+    pAnim->SetMaxPktsPerTraceFile(1000000);
 
-  return 0;
+    Simulator::Run();
+    Simulator::Destroy();
+
+    return 0;
 }
