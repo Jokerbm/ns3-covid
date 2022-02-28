d53ff27f9751e13d5b2f2c14f81b6bdea7bd6e99
Jokerbm <37797569+Jokerbm@users.noreply.github.com>
2022-02-28 21:19:28 +0700

    Update test1.cc

--- test1.cc
+++ test1.cc
@@ -270,11 +270,11 @@ int main(int argc, char *argv[])
     people.setMobility();
 
     // ตั้ง udp client ตัวแพร่เชื้อ -> คนที่ติดเชื้อ
-    int arr_size = sizeof(infected_list) / sizeof(infected_list[0]);
-    for (int i = 0; i < arr_size; i++)
-    {
-        people.setUDPClient(infected_list[i], Seconds(0.0));
-    }
+    // int arr_size = sizeof(infected_list) / sizeof(infected_list[0]);
+    // for (int i = 0; i < arr_size; i++)
+    // {
+    //     people.setUDPClient(infected_list[i], Seconds(0.0));
+    // }
 
     // #define PEOPLE_SHINOVAC 7
     // #define PEOPLE_ASTRA 2
